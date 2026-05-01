7e9fee4b6190585635f5f3b6b360eb5d331d15ec
Kacper <kogutkacper7@gmail.com>
2026-05-01 14:26:08 +0200

    add serializer and implement 2 functions 'serialize_car_object' and 'deserialize_car_object'

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,23 @@
 from rest_framework import serializers
+from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    manufacture = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(min_value=1, max_value=1914)
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+
+
+    def create(self, validated_data):
+        return Car(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacture = validated_data.get("manufacture", instance.manufacture)
+        instance.model = validated_data.get("model", instance.model)
+        instance.horse_powers = validated_data.get("horse_powers", instance.horse_powers)
+        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
+        instance.problem_description = validated_data.get("problem_description", instance.problem_description)
+
+        return instance
